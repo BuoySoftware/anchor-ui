e6c1ae0707a8a7c7ee58937c8af7b81d605d032d
Damian Galarza <galarza.d@gmail.com>
2022-12-23 11:16:51 -0500

    Add layout and nav to anchor-ui

--- packages/components/anchor-ui/src/index.ts
+++ packages/components/anchor-ui/src/index.ts
@@ -1,3 +1,4 @@
 export * from "@buoysoftware/anchor-layout";
+export * from "@buoysoftware/anchor-nav";
 export * from "@buoysoftware/anchor-theme";
 export * from "@buoysoftware/anchor-typography";
