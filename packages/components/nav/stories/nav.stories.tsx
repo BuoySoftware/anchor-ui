6e427baef6fb18264950edd57e83e113b05c47bf
Damian Galarza <galarza.d@gmail.com>
2023-01-03 11:28:43 -0500

    Active state for nav items

--- packages/components/nav/stories/nav.stories.tsx
+++ packages/components/nav/stories/nav.stories.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { ThemeProvider, createGlobalStyle } from "styled-components";
 import { theme } from "@buoysoftware/anchor-theme";
+import { Box } from "@buoysoftware/anchor-layout";
 
 import { Nav, NavLogo, NavSeparator, NavItemsContainer, NavItem } from "../src";
 
@@ -31,18 +33,34 @@ export const Basic = (args) => {
 };
 
 export const Tabs = (args) => {
+  const [currentPage, setCurrentPage] = useState("page1");
+  const onClick = (e): void => {
+    e.preventDefault();
+    const href = e.currentTarget.getAttribute("href");
+    setCurrentPage(href.replace("#", ""));
+  };
+
   return (
-    <Nav>
-      <NavLogo />
-      <NavSeparator />
-      <NavItemsContainer>
-        <NavItem>
-          <a href="#page1">Page 1</a>
-        </NavItem>
-        <NavItem>
-          <a href="#page2">Page 2</a>
-        </NavItem>
-      </NavItemsContainer>
-    </Nav>
+    <Box>
+      <Nav>
+        <NavLogo />
+        <NavSeparator />
+        <NavItemsContainer>
+          <NavItem active={currentPage == "page1"}>
+            <a onClick={onClick} href="#page1">
+              Page 1
+            </a>
+          </NavItem>
+          <NavItem active={currentPage == "page2"}>
+            <a onClick={onClick} href="#page2">
+              Page 2
+            </a>
+          </NavItem>
+        </NavItemsContainer>
+      </Nav>
+
+      {currentPage == "page1" && <Box p="xl">Page 1</Box>}
+      {currentPage == "page2" && <Box p="xl">Page 2</Box>}
+    </Box>
   );
 };
