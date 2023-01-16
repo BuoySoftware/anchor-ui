03aae02f91571f978191ef1c386bce85e0bb3db6
Damian Galarza <galarza.d@gmail.com>
2023-01-16 14:51:33 -0500

    Add sticky footer to anchor-ui

--- packages/anchor-ui/src/index.ts
+++ packages/anchor-ui/src/index.ts
@@ -4,6 +4,7 @@ export * from "@buoysoftware/anchor-dropdown-menu";
 export * from "@buoysoftware/anchor-form";
 export * from "@buoysoftware/anchor-layout";
 export * from "@buoysoftware/anchor-nav";
+export * from "@buoysoftware/anchor-sticky-footer";
 export * from "@buoysoftware/anchor-theme";
 export * from "@buoysoftware/anchor-typography";
 export * from "@buoysoftware/anchor-use-translated-options";
