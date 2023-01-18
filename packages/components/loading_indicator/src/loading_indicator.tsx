192e80bcbd5a68c18967a643e7c41caf2c99b95b
Damian Galarza <galarza.d@gmail.com>
2023-01-18 13:47:03 -0500

    Introduce LoadingIndicator component

---
+++ packages/components/loading_indicator/src/loading_indicator.tsx
@@ -0,0 +1,29 @@
+import styled, { keyframes } from "styled-components";
+import { themeGet } from "@styled-system/theme-get";
+import { Box, BoxProps } from "@buoysoftware/anchor-layout";
+
+const rotation = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+interface LoadingIndicatorProps
+  extends Omit<BoxProps, "color" | "size">,
+    Required<Pick<BoxProps, "color">>,
+    Required<Pick<BoxProps, "size">> {
+  strokeSize: number;
+}
+
+export const LoadingIndicator = styled(Box)<LoadingIndicatorProps>`
+  border: ${(props) => props.strokeSize}px solid
+    ${(props) => themeGet(props.color)};
+  border-bottom-color: transparent;
+  border-radius: 50%;
+  display: inline-block;
+  box-sizing: border-box;
+  animation: ${rotation} 1s linear infinite;
+`;
