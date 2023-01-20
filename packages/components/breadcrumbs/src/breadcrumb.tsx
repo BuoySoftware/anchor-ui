99fce676bc8e1d1bda8b04df9b20bf2dda3e9097
cornshuckin2 <43525061+cornshuckin2@users.noreply.github.com>
2023-01-20 14:52:48 -0500

    Add data-testid to breadcrumb (#40)

--- packages/components/breadcrumbs/src/breadcrumb.tsx
+++ packages/components/breadcrumbs/src/breadcrumb.tsx
@@ -2,15 +2,23 @@ import { Body } from "@buoysoftware/anchor-typography";
 
 interface OwnProps {
   children: React.ReactNode;
+  "data-testid"?: string;
 }
 
 type BreadcrumbProps = OwnProps;
 
 export const Breadcrumb: React.FC<BreadcrumbProps> = ({
   children,
+  "data-testid": testId = "breadcrumb",
 }): React.ReactElement => {
   return (
-    <Body className="breadcrumb" as="li" color="text.secondary" size="s">
+    <Body
+      className="breadcrumb"
+      as="li"
+      color="text.secondary"
+      data-testid={testId}
+      size="s"
+    >
       {children}
     </Body>
   );
