ba7e13ebaff06869006cedac204f353f394c14e0
Damian Galarza <galarza.d@gmail.com>
2023-01-27 20:45:25 -0500

    Introduce TextButton component

---
+++ packages/components/button/src/text_button.tsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import { Heading, HeadingProps } from "@buoysoftware/anchor-typography";
+
+interface OwnProps {
+  children?: React.ReactNode;
+}
+
+type TextButtonProps = OwnProps &
+  Omit<HeadingProps, "as" | "size" | "background" | "color">;
+
+export const TextButton = styled(Heading).attrs(() => ({
+  as: "button",
+  size: "s",
+}))<TextButtonProps>`
+  background: transparent;
+  border: none;
+  height: 28px;
+  color: ${({ theme }) => theme.colors.interactive.default};
+  cursor: pointer;
+  padding: ${({ theme }) => theme.space.xxs};
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.interactive.hover};
+  }
+
+  &:active {
+    color: ${({ theme }) => theme.colors.interactive.pressed};
+  }
+
+  &:focus-visible {
+    border: 1px solid ${({ theme }) => theme.colors.border.focus};
+  }
+
+  &[disabled] {
+    color: ${({ theme }) => theme.colors.text.tertiary};
+  }
+`;
