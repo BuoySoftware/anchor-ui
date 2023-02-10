a251f650badebc2d8a0da430f59060618138e952
Damian Galarza <galarza.d@gmail.com>
2023-02-09 20:09:29 -0500

    Add TextField component from BuoyRails

--- packages/components/form/src/index.ts
+++ packages/components/form/src/index.ts
@@ -7,6 +7,7 @@ export * from "./label";
 export * from "./radio_field";
 export * from "./select";
 export * from "./select_field";
+export * from "./text_field";
 export * from "./use_form_field";
 
 export { default as anchorFormsEn } from "./i18n/anchorForms.en.json";
