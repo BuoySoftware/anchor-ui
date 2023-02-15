ff3e9fa8acbcc93ef201c894bebf735a17bc4584
Damian Galarza <galarza.d@gmail.com>
2023-02-15 14:32:20 -0500

    Expose BoxProps to FormBaseErrors
    This allows someone to adjust the layout of the component if needed.

--- packages/components/form/src/form_base_errors.tsx
+++ packages/components/form/src/form_base_errors.tsx
@@ -1,12 +1,23 @@
+import { BoxProps } from "@buoysoftware/anchor-layout";
+
 import { useFormScope } from "./form_scope_provider";
 import { ErrorMessage } from "./error_message";
 
-export const FormBaseErrors: React.FC = (): React.ReactElement | null => {
+type FormBaseErrorsProps = BoxProps;
+
+export const FormBaseErrors: React.FC<FormBaseErrorsProps> = (
+  props
+): React.ReactElement | null => {
   const { baseErrors, formId } = useFormScope();
 
   if (baseErrors.length === 0) return null;
 
   return (
-    <ErrorMessage errors={baseErrors} parentId={formId} mb="form.fieldGap" />
+    <ErrorMessage
+      errors={baseErrors}
+      parentId={formId}
+      mb="form.fieldGap"
+      {...props}
+    />
   );
-}
+};
