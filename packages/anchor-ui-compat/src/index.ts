8fb20375aaf36b008a57f657e9a7cb928d90b248
Will Murray <will.murray@gmail.com>
2023-02-23 14:02:04 -0500

    Introducing LegacyPageFooter component
    * This component is used widely across BuoyRails as the footer for the
      Donor flow, usually containing buttons for save/continue/cancel. As we
      already have a new PageTemplate component, this will be a compatibility
      bridge to leverage the new theme until such time as we refactor to
      using the new PageTemplate

--- packages/anchor-ui-compat/src/index.ts
+++ packages/anchor-ui-compat/src/index.ts
@@ -1,3 +1,4 @@
 export * from "@buoysoftware/anchor-ui-legacy-button";
 export * from "@buoysoftware/anchor-ui-legacy-layout";
+export * from "@buoysoftware/anchor-ui-legacy-page-footer";
 export * from "@buoysoftware/anchor-ui-legacy-text";
