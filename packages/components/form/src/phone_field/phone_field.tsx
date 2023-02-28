e07ac8df261f1151194dbfdd49f3bb91d8b6aaa2
Will Murray <will.murray@gmail.com>
2023-02-28 18:37:43 -0500

    Adding PhoneNumber field WIP
    * To Do: Add translation for invalid error

---
+++ packages/components/form/src/phone_field/phone_field.tsx
@@ -0,0 +1,81 @@
+import {
+  // get,
+  useFormContext,
+  Controller,
+  RegisterOptions,
+} from "react-hook-form";
+import snakeCase from "lodash/snakeCase";
+import {
+  isPossiblePhoneNumber,
+  parsePhoneNumberFromString,
+} from "libphonenumber-js";
+
+import { FormField } from "../form_field";
+import { useFormField } from "../use_form_field";
+import PhoneInput, { PhoneInputProps } from "./phone_input";
+
+interface OwnProps {
+  defaultValue?: string;
+  label?: Maybe<string>;
+  name: string;
+  rules?: Exclude<RegisterOptions, "valueAsNumber" | "valueAsDate">;
+}
+
+export type PhoneFieldProps = OwnProps &
+  Omit<PhoneInputProps, "onChange" | "defaultValue">;
+
+const formattedValue = (value: string): string => {
+  const parsedPhoneNumber = parsePhoneNumberFromString(value);
+  return parsedPhoneNumber?.formatNational() ?? "";
+};
+
+export const PhoneField: React.FC<PhoneFieldProps> = ({
+  defaultValue = "",
+  label,
+  name,
+  rules,
+  ...inputProps
+}): React.ReactElement => {
+  const { control } = useFormContext();
+  const { error, label: formFieldLabel } = useFormField({
+    name,
+    buildLabel: label === undefined,
+  });
+  // const error = get(errors, name);
+  const fieldLabel = label ?? formFieldLabel;
+  const inputId = snakeCase(name);
+  const placeholder = "";
+
+  const phoneNumberValidation = (phoneNumber: string): string | undefined => {
+    const valid = isPossiblePhoneNumber(phoneNumber, "US");
+
+    if (!valid) return "Invalid phone number" || undefined;
+  };
+
+  return (
+    <FormField
+      error={error}
+      label={fieldLabel}
+      inputId={inputId}
+      name={name}
+      {...inputProps}
+    >
+      <Controller
+        control={control}
+        name={name}
+        defaultValue={formattedValue(defaultValue)}
+        render={({ field }) => {
+          return (
+            <PhoneInput
+              id={inputId}
+              placeholder={placeholder}
+              {...field}
+              {...inputProps}
+            />
+          );
+        }}
+        rules={{ ...rules, validate: { phoneNumber: phoneNumberValidation } }}
+      />
+    </FormField>
+  );
+};
