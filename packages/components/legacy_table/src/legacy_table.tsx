5b4f8347b45d71b30cc779105ab2338636153ffc
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2023-03-03 11:00:36 -1000

    add t prop to legacy-table

--- packages/components/legacy_table/src/legacy_table.tsx
+++ packages/components/legacy_table/src/legacy_table.tsx
@@ -2,6 +2,7 @@ import { BoxProps } from "@buoysoftware/anchor-layout";
 import { CellProps, Table, TableCellConfig, TableProps } from "@buoysoftware/anchor-table";
 import { LegacyTextVariant } from "@buoysoftware/anchor-ui-legacy-text";
 import { TFunction } from "i18next";
+import get from "lodash/get";
 import snakeCase from "lodash/snakeCase";
 import { useTranslation } from "react-i18next";
 
@@ -9,22 +10,29 @@ type LegacyTextProps = BoxProps & { variant?: LegacyTextVariant };
 type LegacyCellProps = Partial<LegacyTextProps> | undefined;
 
 type CellT = TFunction<string[], undefined, string[]>;
+type CellTP = Parameters<CellT>;
 
-type LegacyTableCellConfig<RecordData> = Omit<TableCellConfig<RecordData>, 'cellProps' | 'render'> & {
+export type LegacyTableCellConfig<RecordData> = Omit<TableCellConfig<RecordData>, 'cellProps' | 'render'> & {
   cellProps?: (rowData: RecordData) => LegacyCellProps;
   render?: (
     rowData: RecordData,
     tableT: TFunction,
-    cellT: (...params: Parameters<CellT>) => string
+    cellT: ReturnType<typeof keyPrefixedCellT>
   ) => React.ReactNode;
+  t?: TFunction<string, string | null>;
   translationNamespace?: string;
   translationKeyPrefix?: string;
 };
 
-const keyPrefixedCellT = (cellT: CellT, keyPrefix: string) => (...params: Parameters<CellT>) => {
-  const [firstParam, ...restParams] = params;
-  const prefixedParam = typeof firstParam === "string" ? [keyPrefix, firstParam].join(".") : firstParam.map(key => [keyPrefix, key].join("."));
-  return cellT(prefixedParam, ...restParams);
+const keyPrefixedCellT = (cellT: CellT, keyPrefix: string) => (key: CellTP[0], defaultValueOrOptions?: string | CellTP[1], options?: CellTP[1]) => {
+  const prefixedKey = typeof key === "string" ? [keyPrefix, key].join(".") : key.map(value => [keyPrefix, value].join("."));
+  if (typeof defaultValueOrOptions === "string") {
+    return cellT(prefixedKey, defaultValueOrOptions, options);
+  } else if (typeof defaultValueOrOptions === "object") {
+    return cellT(prefixedKey, defaultValueOrOptions);
+  } else {
+    return cellT(prefixedKey);
+  }
 }
 
 export type LegacyTableProps<RecordData> = Omit<TableProps<RecordData>, 'cellConfigs'> & {
@@ -79,6 +87,7 @@ const updateCellProps = (props: LegacyCellProps | undefined): CellProps | undefi
   }
 }
 
+const defaultRenderPlaceholder = (): string => "--";
 
 export const LegacyTable = <RecordData,>({ cellConfigs, ...props }: LegacyTableProps<RecordData>): React.ReactElement => {
   const i18nKey = snakeCase(props.name);
@@ -86,13 +95,22 @@ export const LegacyTable = <RecordData,>({ cellConfigs, ...props }: LegacyTableP
   const { t: tableT } = useTranslation("tables", { keyPrefix: i18nKey });
   const translationNamespaces = [...new Set(cellConfigs.map(({ translationNamespace }) => translationNamespace ?? "shared").filter(Boolean))];
   const namespaces = translationNamespaces.length > 0 ? translationNamespaces : undefined;
-  const { t: cellT } = useTranslation(namespaces);
+  const { t: unprefixedCellT } = useTranslation(namespaces);
   const actualT = tableT ?? t;
+  const renderPlaceholder = props.renderPlaceholder ?? defaultRenderPlaceholder;
 
   return <Table cellConfigs={cellConfigs.map(({ cellProps, dataKey, name, translationKeyPrefix, ...cellConfig }) => {
-    const render = cellConfig.render ? (rowData: RecordData) => {
-      const actualCellT = translationKeyPrefix ? keyPrefixedCellT(cellT, translationKeyPrefix) : cellT;
-      return cellConfig.render && cellConfig.render(rowData, actualT, actualCellT);
+    const render = cellConfig.render || translationKeyPrefix || cellConfig.t ? (record: RecordData): React.ReactNode => {
+      const value = get(record, dataKey, undefined);
+      const hasNoValue = value === null || value === undefined;
+      const cellT = translationKeyPrefix ? keyPrefixedCellT(unprefixedCellT, value) : unprefixedCellT;
+
+      if (cellConfig.render) return cellConfig.render(record, actualT, cellT);
+      if (hasNoValue) return renderPlaceholder(record);
+      if (translationKeyPrefix) return cellT(value, { defaultValue: value });
+      if (cellConfig.t) return cellConfig.t(value, { defaultValue: value });
+
+      return value;
     } : undefined;
 
     return { dataKey, render, name, cellProps: (recordData: RecordData) => updateCellProps(cellProps?.(recordData)) };
