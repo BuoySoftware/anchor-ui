a0c7ea5c516e54947d8d49a613f50863cba0fb7e
Will Murray <will.murray@gmail.com>
2023-03-07 11:46:20 -0500

    Updating package.json/index.ts

--- packages/anchor-ui-compat/src/index.ts
+++ packages/anchor-ui-compat/src/index.ts
@@ -2,3 +2,4 @@ export * from "@buoysoftware/anchor-ui-legacy-button";
 export * from "@buoysoftware/anchor-ui-legacy-layout";
 export * from "@buoysoftware/anchor-ui-legacy-page-footer";
 export * from "@buoysoftware/anchor-ui-legacy-text";
+export * from "@buoysoftware/anchor-ui-legacy-trans";
