31f98d951affc70ef68b32a90fefdc945cd3fb1f
Damian Galarza <galarza.d@gmail.com>
2023-03-08 16:19:21 -0500

    Properly support scrolling within AppTemplate
    Previously, if content within the app-content area caused scrolling, the
    page would scroll but the side-nav would disappear as you scrolled. This updates
    the AppTemplate to extract 2 discrete components for the grid area, allowing
    us to ensure proper scrolling in the app-content area while preserving
    the side nav.
    
    Co-authored-by: Will Murray <will@buoysoftware.com>

---
+++ packages/components/app_template/src/app_side_nav.tsx
@@ -0,0 +1,18 @@
+import { Box, BoxProps } from "@buoysoftware/anchor-layout";
+
+interface OwnProps {
+  children?: React.ReactNode;
+}
+
+type AppSideNavProps = OwnProps & Omit<BoxProps, "gridArea">;
+
+export const AppSideNav: React.FC<AppSideNavProps> = ({
+  children,
+  ...props
+}): React.ReactElement => {
+  return (
+    <Box gridArea="side-nav" {...props}>
+      {children}
+    </Box>
+  );
+};
