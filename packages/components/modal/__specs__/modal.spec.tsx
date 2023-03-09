d8de0f2bec3f3c37f317e17721f374843b1938d0
Will Murray <will.murray@gmail.com>
2023-03-09 10:46:22 -0500

    Updating jest config and spec for Modal

--- packages/components/modal/__specs__/modal.spec.tsx
+++ packages/components/modal/__specs__/modal.spec.tsx
@@ -1,4 +1,3 @@
-import { Box } from "@buoysoftware/anchor-layout";
 import { Button } from "@buoysoftware/anchor-button";
 
 import { Modal } from "../src/modal";
@@ -7,24 +6,23 @@ import { render } from "../test_utils";
 describe("<Modal />", () => {
   it("renders correctly", () => {
     const modal = render(
-      <Box id="root">
-        <Modal
-          isOpen={true}
-          modalActions={
-            <>
-              <Button onClick={jest.fn()} colorScheme="basic" mr="xxs">
-                Cancel
-              </Button>
-              <Button onClick={jest.fn()} colorScheme="primary">
-                Save
-              </Button>
-            </>
-          }
-          onRequestClose={jest.fn()}
-          testId="test-modal"
-          title="Test Modal"
-        />
-      </Box>
+      <Modal
+        ariaHideApp={false}
+        isOpen={true}
+        modalActions={
+          <>
+            <Button onClick={jest.fn()} colorScheme="basic" mr="xxs">
+              Cancel
+            </Button>
+            <Button onClick={jest.fn()} colorScheme="primary">
+              Save
+            </Button>
+          </>
+        }
+        onRequestClose={jest.fn()}
+        testId="test-modal"
+        title="Test Modal"
+      />
     );
 
     expect(modal.asFragment()).toMatchSnapshot();
