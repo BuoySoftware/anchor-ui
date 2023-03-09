9d017d1dfef4f308ac24c32d54dc10fc516b2922
Will Murray <will.murray@gmail.com>
2023-03-09 17:58:15 -0500

    Adding legacy_modal to anchor-ui-compat

--- packages/anchor-ui-compat/src/index.ts
+++ packages/anchor-ui-compat/src/index.ts
@@ -1,5 +1,6 @@
 export * from "@buoysoftware/anchor-ui-legacy-button";
 export * from "@buoysoftware/anchor-ui-legacy-layout";
+export * from "@buoysoftware/anchor-ui-legacy-modal";
 export * from "@buoysoftware/anchor-ui-legacy-page-footer";
 export * from "@buoysoftware/anchor-ui-legacy-connection-table";
 export * from "@buoysoftware/anchor-ui-legacy-table";
