fc8c1019fe65be0a6942a9b8eadb29f98dc46db1
Damian Galarza <galarza.d@gmail.com>
2023-03-09 22:26:50 -0500

    Update PageHeader to support children
    Example use case is to display a badge as part of the PageHeader.

--- packages/components/page_template/src/page_header.tsx
+++ packages/components/page_template/src/page_header.tsx
@@ -2,12 +2,14 @@ import { Flex, FlexProps } from "@buoysoftware/anchor-layout";
 import { Heading } from "@buoysoftware/anchor-typography";
 
 interface OwnProps {
+  children?: React.ReactNode;
   title: string;
 }
 
 type PageHeaderProps = OwnProps & Omit<FlexProps, "gridArea">;
 
 export const PageHeader: React.FC<PageHeaderProps> = ({
+  children,
   title,
   ...props
 }): React.ReactElement => {
@@ -19,11 +21,13 @@ export const PageHeader: React.FC<PageHeaderProps> = ({
       mb="l"
       ml="page.gutterX"
       mt="xxxl"
+      alignItems="center"
       {...props}
     >
-      <Heading as="h1" size="xl">
+      <Heading as="h1" mr="xs" size="xl">
         {title}
       </Heading>
+      {children}
     </Flex>
   );
 };
