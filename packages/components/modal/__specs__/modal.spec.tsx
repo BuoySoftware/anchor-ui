e5faa601e143554cd3bf9b33c67aa5f5ab5d6d1f
Will Murray <will.murray@gmail.com>
2023-03-15 14:53:38 -0400

    Fixing modal specs

--- packages/components/modal/__specs__/modal.spec.tsx
+++ packages/components/modal/__specs__/modal.spec.tsx
@@ -1,52 +1,35 @@
-import Modal from "react-modal";
 import { Button } from "@buoysoftware/anchor-button";
 
-import { Modal as AnchorModal } from "../src/modal";
+import { Modal } from "../src/modal";
 import { render } from "../test_utils";
 
-// const MockModal: React.FC = (): React.ReactElement => {
-//   return (
-//     <AnchorModal
-//       ariaHideApp={false}
-//       isOpen={true}
-//       modalActions={
-//         <>
-//           <Button onClick={jest.fn()} colorScheme="basic" mr="xxs">
-//             Cancel
-//           </Button>
-//           <Button onClick={jest.fn()} colorScheme="primary">
-//             Save
-//           </Button>
-//         </>
-//       }
-//       testId="test-modal"
-//       title="Test Modal"
-//     />
-//   );
-// };
+const MockModal: React.FC = (): React.ReactElement => {
+  return (
+    <Modal
+      ariaHideApp={false}
+      isOpen={true}
+      modalActions={
+        <>
+          <Button onClick={jest.fn()} colorScheme="basic" mr="xxs">
+            Cancel
+          </Button>
+          <Button onClick={jest.fn()} colorScheme="primary">
+            Save
+          </Button>
+        </>
+      }
+      testId="test-modal"
+      title="Test Modal"
+    >
+      Test modal contents
+    </Modal>
+  );
+};
 
 describe("<Modal />", () => {
-  it("renders correctly", async () => {
-    const modal = render(
-      <AnchorModal
-        isOpen={true}
-        modalActions={
-          <>
-            <Button onClick={jest.fn()} colorScheme="basic" mr="xxs">
-              Cancel
-            </Button>
-            <Button onClick={jest.fn()} colorScheme="primary">
-              Save
-            </Button>
-          </>
-        }
-        testId="test-modal"
-        title="Test Modal"
-      />
-    );
+  it("renders correctly", () => {
+    const modal = render(<MockModal />);
 
-    await setTimeout(() => {}, 1000);
-
-    expect(modal.asFragment()).toMatchSnapshot();
+    expect(modal.baseElement).toMatchSnapshot();
   });
 });
