e41c5bfa67e0265ec8d3ea37b7c69ea2bde776e2
Anthony Costanzo <memberships@acostanzo.com>
2023-03-23 14:49:23 -0400

    Support forwarding refs to table rows and cells

--- packages/components/table/src/table_row.tsx
+++ packages/components/table/src/table_row.tsx
@@ -1,36 +1,53 @@
 import kebabCase from "lodash/kebabCase";
+import { CSSProperties, forwardRef } from "react";
 
 import { Td } from "./td";
 import { TableRow as GeneralRowProps } from "./types";
 
-type TableRowProps<RecordData> = GeneralRowProps<RecordData>;
+interface OwnProps {
+  style?: CSSProperties;
+  className?: string;
+}
 
-export const TableRow = function <RecordData>({
-  cellConfigs,
-  record,
-  rowAction,
-  rowId,
-  ...tableRowProps
-}: TableRowProps<RecordData>): React.ReactElement {
+export type TableRowProps<RecordData> = OwnProps & GeneralRowProps<RecordData>;
+
+function PreForwardedTableRow<RecordData>(
+  {
+    TableCellComponent = Td,
+    cellConfigs,
+    record,
+    rowAction,
+    rowId,
+    style,
+    renderPlaceholder,
+    ...tableRowProps
+  }: TableRowProps<RecordData>,
+  ref?: React.ForwardedRef<HTMLTableRowElement>
+) {
   return (
     <tr
       data-testid={rowId}
+      ref={ref}
+      style={{ ...style }}
       {...(rowAction ? { onClick: () => rowAction(record) } : {})}
+      {...tableRowProps}
     >
       {cellConfigs.map((cellConfig) => {
         const { dataKey } = cellConfig;
         const cellId = `${rowId}-td-${kebabCase(String(dataKey))}`;
 
         return (
-          <Td
+          <TableCellComponent
             cellConfig={cellConfig}
             key={cellId}
             record={record}
             testId={cellId}
-            {...tableRowProps}
+            renderPlaceholder={renderPlaceholder}
           />
         );
       })}
     </tr>
   );
-};
+}
+
+export const TableRow = forwardRef(PreForwardedTableRow)
