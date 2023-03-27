ec38340e9325817b19e349877865bd859d993a25
Anthony Costanzo <memberships@acostanzo.com>
2023-03-27 16:02:00 -0400

    Fix sortable table record caching
    The sortable table was not re-rendering when records changed. This
    update adds an effect for updating the sortedRecords state when records
    is changed externally.
    
    Additionally, this updates the logic for firing the onSort to be more
    performant and predictable.

--- packages/components/table/__specs__/sortable_table.spec.tsx
+++ packages/components/table/__specs__/sortable_table.spec.tsx
@@ -4,38 +4,44 @@ import { SortableTable } from "../src/sortable_table";
 import { SortableTableCellConfig } from "../src/types";
 import { act, render } from "../test_utils";
 import { initReactI18next } from "react-i18next";
+import { useCallback, useState } from "react";
 
-interface Donation {
+interface Person {
   __typename: string;
   id: string;
-  status: string;
+  name: string;
+  position: number;
 }
 
-const cellConfigs: SortableTableCellConfig<Donation>[] = [
-  { dataKey: "id", sortHandle: true },
-  { dataKey: "status" },
+const cellConfigs: SortableTableCellConfig<Person>[] = [
+  { dataKey: "position", sortHandle: true },
+  { dataKey: "name" },
 ];
 
-const records: Donation[] = [
+const records: Person[] = [
   {
-    __typename: "Donation",
+    __typename: "Person",
     id: "1",
-    status: "Good",
+    name: "Jerry Seinfeld",
+    position: 1,
   },
   {
-    __typename: "Donation",
+    __typename: "Person",
     id: "2",
-    status: "Bad",
+    name: "George Costanza",
+    position: 2,
   },
   {
-    __typename: "Donation",
+    __typename: "Person",
     id: "3",
-    status: "Ugly",
+    name: "Elaine Benes",
+    position: 3,
   },
   {
-    __typename: "Donation",
+    __typename: "Person",
     id: "4",
-    status: "Other",
+    name: "Cosmo Kramer",
+    position: 4,
   },
 ];
 
@@ -43,8 +49,8 @@ describe("<SortableTable />", () => {
   const tables = {
     donations: {
       th: {
-        id: "ID",
-        status: "Status",
+        position: "Position",
+        name: "Full Name",
       },
     },
   };
@@ -73,8 +79,58 @@ describe("<SortableTable />", () => {
     expect(component.asFragment()).toMatchSnapshot();
   });
 
-  describe("sorting by mouse", () => {
-    it("fires callback with sorted items", async () => {
+  it("re-renders when records change", async () => {
+    const DummyComponent: React.FC = (): React.ReactElement => {
+      const [sortedRecords, setSortedRecords] = useState<Person[]>(records);
+
+      const reverseRows = useCallback(() => {
+        setSortedRecords((records) => [...records].reverse())
+      }, [])
+
+      return (
+        <>
+          <SortableTable
+            cellConfigs={cellConfigs}
+            name="donations"
+            recordIdKey="id"
+            records={sortedRecords}
+          />
+          <a href="#" onClick={reverseRows}>Reverse Rows</a>
+        </>
+      );
+    };
+    const user = userEvent.setup();
+    const component = render(<DummyComponent />);
+
+    expect(
+      component.queryAllByRole("button").map((element) => element.textContent)
+    ).toEqual(["1", "2", "3", "4"]);
+
+    const reverseRowsButton = component.getByText("Reverse Rows");
+    await act(() => user.click(reverseRowsButton));
+
+    expect(
+      component.queryAllByRole("button").map((element) => element.textContent)
+    ).toEqual(["4", "3", "2", "1"]);
+  });
+
+  describe("onSort", () => {
+    it("does not fire on initial render", () => {
+      const onSort = jest.fn();
+      render(
+        <SortableTable
+          cellConfigs={cellConfigs}
+          name="donations"
+          onSort={onSort}
+          recordIdKey="id"
+          records={records}
+        />
+      );
+
+      expect(onSort).not.toHaveBeenCalled();
+    });
+
+    it("fires when sorted by mouse", async () => {
       const user = userEvent.setup();
       const onSort = jest.fn();
       const component = render(
@@ -88,20 +144,18 @@ describe("<SortableTable />", () => {
       );
       const rows = component.queryAllByRole("button");
 
-      await act(() => user.pointer({ keys: "[MouseLeft>]", target: rows[3] }))
-      await act(() => user.pointer({ keys: "[/MouseLeft]", target: rows[0] }))
+      await act(() => user.pointer({ keys: "[MouseLeft>]", target: rows[3] }));
+      await act(() => user.pointer({ keys: "[/MouseLeft]", target: rows[0] }));
 
       expect(onSort).toHaveBeenCalledWith([
         records[3],
         records[0],
         records[1],
-        records[2]
-      ])
-    })
-  });
+        records[2],
+      ]);
+    });
 
-  describe("sorting by keyboard", () => {
-    it("fires callback with sorted items", async () => {
+    it("fires when sorted by keyboard", async () => {
       const user = userEvent.setup();
       const onSort = jest.fn();
       render(
@@ -114,19 +168,19 @@ describe("<SortableTable />", () => {
         />
       );
 
-      await act(() => user.keyboard("{Shift>}[Tab]{/Shift}")) // Focus last row
-      await act(() => user.keyboard("[Space]")) // Activate "Drag" Start
-      await act(() => user.keyboard("[ArrowUp]")) // "Drag" up
-      await act(() => user.keyboard("[ArrowUp]")) // "Drag" up
-      await act(() => user.keyboard("[ArrowUp]")) // "Drag" up
-      await act(() => user.keyboard("[Space]")) // End "Drag"
+      await act(() => user.keyboard("{Shift>}[Tab]{/Shift}")); // Focus last row
+      await act(() => user.keyboard("[Space]")); // Activate "Drag" Start
+      await act(() => user.keyboard("[ArrowUp]")); // "Drag" up
+      await act(() => user.keyboard("[ArrowUp]")); // "Drag" up
+      await act(() => user.keyboard("[ArrowUp]")); // "Drag" up
+      await act(() => user.keyboard("[Space]")); // End "Drag"
 
       expect(onSort).toHaveBeenCalledWith([
         records[3],
         records[0],
         records[1],
-        records[2]
-      ])
-    })
+        records[2],
+      ]);
+    });
   });
 });
