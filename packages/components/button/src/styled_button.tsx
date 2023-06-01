8cb1f28351b4fdd139c493058ed1e4120be35ed0
Pedro Simon <simonpedro.est@gmail.com>
2023-06-01 10:43:36 -0300

    Make Buttom component polymorphic
    Now the Button component supports the `as` prop, which allows
    changing the underlined root component used. This means now we
    can do things like `<Button as={Link} to="/some-path" ...`.
    
    Also, as part of this changeset:
    - Decouple loading indicator from type prop: The loading indicator
      is now displayed for all button types, not just submit buttons.
      The responsibility of determining when to show the
      ploading indicator belongs to the consumer now.
    - Remove `InnerButton` component from button: `InnerButton` styles are
      merged with root `StyledButton` component, so we can spread out the
      rest props to the root element. It also provides a more flat html
      structure, and fixes a bug in which clicking outside the visible
      border area would also trigger a click event.

--- packages/components/button/src/styled_button.tsx
+++ packages/components/button/src/styled_button.tsx
@@ -1,24 +1,97 @@
 import styled from "styled-components";
+import { Flex } from "@buoysoftware/anchor-layout";
+import get from "lodash/get";
+import { ThemeProps } from "@buoysoftware/anchor-theme";
 
-import { boxCss, BoxProps } from "@buoysoftware/anchor-layout";
+export type ColorScheme = "basic" | "primary" | "critical";
 
-type StyledButtonProps = BoxProps;
+interface OwnProps {
+  colorScheme: ColorScheme;
+}
 
-export const StyledButton = styled.button<StyledButtonProps>`
-  ${boxCss};
+type StyledButtonProps = OwnProps;
 
-  margin: 0;
-  padding: 0;
-  display: flex;
+interface ButtonColors {
+  active: string;
+  border: string;
+  default: string;
+  disabledBackground: string;
+  disabledBorder: string;
+  hover: string;
+  text: string;
+}
+
+const BUTTON_COLORS: Record<ColorScheme, ButtonColors> = {
+  basic: {
+    active: "background.pressed",
+    border: "border.default",
+    default: "background.default",
+    disabledBackground: "white",
+    disabledBorder: "border.subdued",
+    hover: "background.hover",
+    text: "text.primary",
+  },
+  primary: {
+    active: "interactive.pressed",
+    border: "transparent",
+    default: "interactive.default",
+    disabledBackground: "grey20",
+    disabledBorder: "transparent",
+    hover: "interactive.hover",
+    text: "white",
+  },
+  critical: {
+    active: "interactive.criticalPressed",
+    border: "transparent",
+    default: "interactive.critical",
+    disabledBackground: "grey20",
+    disabledBorder: "transparent",
+    hover: "interactive.criticalHover",
+    text: "white",
+  },
+};
+
+const getThemeColor = (
+  colorScheme: ColorScheme,
+  theme: ThemeProps,
+  key: keyof ButtonColors
+): string => {
+  const themeKey = BUTTON_COLORS[colorScheme][key];
+  return get(theme.colors, themeKey) || themeKey;
+};
+
+export const StyledButton = styled(Flex)<StyledButtonProps>`
   cursor: pointer;
-  border: 2px solid transparent;
-  background: transparent;
+  border-style: solid;
+  border-width: 1px;
+  background-color: ${(props) =>
+    getThemeColor(props.colorScheme, props.theme, "default")};
+  border-color: ${(props) =>
+    getThemeColor(props.colorScheme, props.theme, "border")};
+  color: ${(props) => getThemeColor(props.colorScheme, props.theme, "text")};
+
+  &:hover {
+    background-color: ${(props) =>
+      getThemeColor(props.colorScheme, props.theme, "hover")};
+  }
+
+  &:active {
+    background-color: ${(props) =>
+      getThemeColor(props.colorScheme, props.theme, "active")};
+  }
+
+  &:disabled {
+    cursor: not-allowed;
+    background-color: ${(props) =>
+      getThemeColor(props.colorScheme, props.theme, "disabledBackground")};
+    border-color: ${(props) =>
+      getThemeColor(props.colorScheme, props.theme, "disabledBorder")};
+    color: ${(props) => props.theme.colors.text.tertiary};
+  }
 
   &:focus-visible {
-    background: rgba(255, 255, 255, 0.01);
     border-radius: 4px;
-    border-color: rgba(255, 255, 255, 0.01);
-    box-shadow: 0px 0px 0px 2px #5b91f4;
-    outline: none;
+    outline: 2px solid #5b91f4;
+    outline-offset: 2px;
   }
 `;
